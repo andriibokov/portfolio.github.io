38e9bbe1a1edda83da75b7194504b108849a6fbc
Andrii Bokov <bokov579@gmail.com>
2020-08-28 15:16:36 +0300

    portfolio

---
+++ myWorks/TodoVue/vue/src/components/Todo.vue
@@ -0,0 +1,15 @@
+<template>
+    <li class="todo__item">
+        <button v-on:click="$emit('remove')">Выполнено</button>
+        <p>{{ todo }}</p>
+        <button v-on:click="$emit('remove')">Редактировать</button>
+        <button v-on:click="$emit('remove-todo')">Удалить</button>
+    </li>    
+</template>
+
+<script>
+
+export default {
+    props: ['todo']
+}
+</script>
