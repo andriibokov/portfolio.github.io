8d174fc7af24498f8f33ee10c4f29c6c0990c1f8
Andrii Bokov <bokov579@gmail.com>
2020-08-29 14:53:28 +0300

    taskTodo add

---
+++ myWorks/Calculator-VueJS/main.js
@@ -0,0 +1,41 @@
+
+// Vue.component('',{
+//     props: [''],
+//     template:`
+//     `
+// })
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        inputResult:'',
+        inputMath:'',
+        numbers:[1, 2, 3, 4, 5, 6, 7, 8, 9, ".", 0],
+        operations:['%', '/', '+', '-', '*'],
+        reset: "CE",
+        equal: "=",
+        checkBox: false
+    },
+    methods: {
+        pushNumber: function (params) {
+            checkBox = false
+            this.inputMath += params
+        },
+        pushOperation: function (params) {
+            !checkBox ? this.inputMath += params : false;
+            checkBox = true
+        },
+        resetInput: function () {
+            checkBox = false
+            this.inputResult = ""            
+            this.inputMath = ""            
+        },
+        equalInput: function () {
+            checkBox = false
+            this.inputResult = eval(this.inputMath)
+        }
+    }
+})
+    
+    
+    
